b8faac433a07bc193c7ad0eac9f8fe587906821f
ye13jian <963552657@qq.com>
2016-05-07 17:54:15 +0800

    调整代码的格式

--- src/main/java/com/jianye/bitOperationMavenDemo/App.java
+++ src/main/java/com/jianye/bitOperationMavenDemo/App.java
@@ -4,10 +4,8 @@ package com.jianye.bitOperationMavenDemo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
